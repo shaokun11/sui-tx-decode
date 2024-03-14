bb25ce84b9512ac2158b4c24fffb0394a2b076d3
shaokun <605416017@qq.com>
2024-03-14 16:13:32 +0800

    增加错误处理和只是解析交易内容

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ import { execa } from "execa";
 // await execa("cargo build", []);
 let tx_data =
     "AAACACBqEB6aOvXIBwES+Ahkizbvv43uihqC3kbZUE6WoRCKFwEAjvdvVsOZYzousxC8qRJOXy84znOeqsu2YAaIgE4HhEgCAAAAAAAAACB9w3+ufZMpihJFwxtCBojBaGy00TVtFxgN2C6TpIPFqwEBAQEBAAEAAAS0l6kWtGVmCaf6gnoJGE1vR2gdO6dM4NejbGSysfiHAZ+Q9/hmzCnfsdpjc86U+dldylpA9OF2mRjuv5+64AvTAgAAAAAAAAAgjleHL0UiRGjh/BfIFHCJ3EMY/dQA22c2TvNQyVJnbYUEtJepFrRlZgmn+oJ6CRhNb0doHTunTODXo2xksrH4hwoAAAAAAAAAoIYBAAAAAAAA";
+tx_data =
+    "AAABAQGJeL4ADwYhIlia2yYMZ+dSd+iNzGC3JqO7RCekGocNbFInDgAAAAAAAQEAcfU1+QF4q9Nvbp3PhpyShGy17zxMK54Vn6eUmqHxvl0HY291bnRlcglpbmNyZW1lbnQAAQEAAGQpV670B1MRv4/oPaQKGstSexun6H/lVkfC68ZQA877AGQpV670B1MRv4/oPaQKGstSexun6H/lVkfC68ZQA8776AMAAAAAAAAAdDukCwAAAAA=";
 let signature =
     "AKD4XdltkCyBi1Heb4EJJ3lzuV3F4u7+CYeaE+Fd7qXpaT17yd4tHWjMf4CWq3TuXBLxTpkc2MV39P6p7eMV8QnqvbuA0Q1Bqu4RHV3JPpqmH+C527hWJGUBOZN1j9sg8w==";
 
-const { stdout } = await execa("./target/debug/sui-transaction-decode.exe", [tx_data, signature]);
-
-console.log(JSON.parse(JSON.parse(stdout)));
+const { stdout } = await execa("./target/debug/sui-transaction-decode.exe", [tx_data, signature]).catch(
+    () => -1
+);
+if (stdout) {
+    console.log(JSON.parse(stdout));
+} else {
+    console.log("error");
+}
--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,28 @@ use fastcrypto_zkp::bn254::zk_login_api::ZkLoginEnv;
 use im::hashmap::HashMap as ImHashMap;
 use std::env;
 use sui_types::signature::VerifyParams;
-use sui_types::transaction::{
-    Transaction, TransactionData
-};
+use sui_types::transaction::{Transaction, TransactionData};
 use sui_types::{crypto::ToFromBytes, signature::GenericSignature};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let tx_base64 = Base64::try_from(String::from(args[1].as_str()))?;
-    let signature_base64 = Base64::try_from(String::from(args[2].as_str()))?;
-
     // let base64_str = "AAAAAQBx9TX5AXir029unc+GnJKEbLXvPEwrnhWfp5SaofG+XQdjb3VudGVyBmNyZWF0ZQAAZClXrvQHUxG/j+g9pAoay1J7G6fof+VWR8LrxlADzvsBMdEp2sqzSN0bexRB7jIUas+xp9UwCLwkRjjZi6CuNO5HJw4AAAAAACCjIIQXqPcO8JQESWTYVdvNQwodBjBnN52Wj5lLy/7qJmQpV670B1MRv4/oPaQKGstSexun6H/lVkfC68ZQA8776AMAAAAAAADwD0YAAAAAAAA=";
     // let tx_base64 = Base64::try_from(String::from(base64_str)).unwrap();
     // let signature_str = "ALJCPPlDe2pc6+Er9Mh/BNQhaK5nScMX0B+Vu674cIQM23GL1WlALg41Vn5km6EwR9dWjQcvkEDof+bSpx2PiwOX/NYXhh+8Elx25+t+Ung/9uoKXk3xB+6n90u5REwfuw==";
     // let signature_base64 = Base64::try_from(String::from(signature_str)).unwrap();
-
     let data: TransactionData = bcs::from_bytes(&tx_base64.to_vec()?)?;
     let data_str = serde_json::to_string(&data.clone())?;
-    let sign = GenericSignature::from_bytes(&signature_base64.to_vec().unwrap()).unwrap();
-    let mut sigs = Vec::new();
-    sigs.push(sign);
-    let txn = Transaction::from_generic_sig_data(data, sigs);
-    let verify_params =
-        VerifyParams::new(ImHashMap::new(), vec![], ZkLoginEnv::default(), true, true);
-    match txn.verify_signature(&verify_params) {
-        Ok(_) => {
-            print!("{:?}", data_str);
-        }
-        Err(_) => {
-            print!("");
-        }
-    };
+    if args.len() == 3 {
+        let signature_base64 = Base64::try_from(String::from(args[2].as_str()))?;
+        let sign = GenericSignature::from_bytes(&signature_base64.to_vec().unwrap()).unwrap();
+        let mut sigs = Vec::new();
+        sigs.push(sign);
+        let txn = Transaction::from_generic_sig_data(data, sigs);
+        let verify_params =
+            VerifyParams::new(ImHashMap::new(), vec![], ZkLoginEnv::default(), true, true);
+        txn.verify_signature(&verify_params).unwrap();
+    }
+    print!("{}", data_str);
     Ok(())
 }
